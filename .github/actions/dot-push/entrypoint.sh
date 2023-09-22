f2a9c2ac7ba25ed1d19a7eb810ae5f7fecb4e6ed
fabrizzio-dotCMS <fabrizzio@dotCMS.com>
2023-09-21 19:33:30 -0600

    debug

--- .github/actions/dot-push/entrypoint.sh
+++ .github/actions/dot-push/entrypoint.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
   echo "Running dot-push action entrypoint.sh"
-  for word in "$@"; do echo "$word"; done
 
   bash /dot-cli/run-java.sh "$@"
   exit_code=$?
 
-  echo "exit code: $exit_code"
   echo "exit_code=$exit_code" >> "$GITHUB_OUTPUT"
 
   echo "Quarkus log file contents:"
